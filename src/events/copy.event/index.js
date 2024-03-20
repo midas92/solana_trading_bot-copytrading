6c94350629853785ed83ae94ec4b12075ef67344
midas92 <bravelancers@gmail.com>
2024-03-21 05:40:54 +1000

    update copyTrade

--- src/events/copy.event/index.js
+++ src/events/copy.event/index.js
@@ -8,7 +8,9 @@ const { findSettings } = require('@/controllers/settings.controller');
 const { coverFee } = require('@/features/fee.feature');
 const { initiateSwap, swapToken } = require('@/features/swap.feature');
 const { confirmTransaction } = require('@/services/solana');
+const { getPair } = require('@/services/dexscreener');
 const {
+  transactionInitiateMsg,
   transactionBuildFailedMsg,
   transactionConfirmedMsg,
   transactionFailedMsg,
@@ -35,8 +37,7 @@ const parseTransaction = async (bot, msg, { copyWalletAddress }) => {
   const preTokenBalances = meta.preTokenBalances;
   if (postTokenBalances.length === 0 || preTokenBalances.length === 0)
     return;
-  console.log('postTokenBalances => ', postTokenBalances)
-  console.log('preTokenBalances => ', preTokenBalances)
+  
 
   let inputMint, outputMint, slippage, amount, mode;
   const isAuto = true;
@@ -45,22 +46,27 @@ const parseTransaction = async (bot, msg, { copyWalletAddress }) => {
   console.log("targetTokneBalances => ", targetTokenBalances)
   if (targetTokenBalances.length <= 0)
     return
-  const postAmount = targetTokenBalances[0].uiTokenAmount.amount;
-  const preAmount = preTokenBalances.filter(one => one.accountIndex === targetTokenBalances[0].accountIndex)[0].uiTokenAmount.amount;
-
+  
+  const postAmount = targetTokenBalances[0].uiTokenAmount.uiAmount;
+  const preAmount = preTokenBalances.filter(one => one.accountIndex === targetTokenBalances[0].accountIndex)[0].uiTokenAmount.uiAmount;
+  
+  const pair = await getPair(targetTokenBalances[0].mint);
+  priceSol = parseFloat(pair.priceNative);
+  amount=(postAmount-preAmount)*priceSol* Math.pow(10, 9)
+  
   if (postAmount === preAmount) {
     return
   } else if (postAmount > preAmount) {
     inputMint = 'So11111111111111111111111111111111111111112';
     outputMint = targetTokenBalances[0].mint;
     slippage = settings.autoBuySlippage;
-    amount = parseInt((postAmount - preAmount) * 0.99);
+    amount = parseInt(amount* 0.99);
     mode = 'buy';
   } else {
     inputMint = targetTokenBalances[0].mint;
     outputMint = 'So11111111111111111111111111111111111111112';
     slippage = settings.autoSellSlippage;
-    amount = parseInt((preAmount - postAmount) / preAmount);
+    amount = parseInt(-amount / preAmount);
     mode = 'sell';
   }
 
@@ -91,65 +97,57 @@ const copyTrade = async (bot, msg, params) => {
   let txid, quoteResponse;
   const payer = Keypair.fromSecretKey(bs58.decode(wallet.secretKey));
 
-  try {
-    const res = await initiateSwap({
-      inputMint,
-      outputMint,
-      amount,
-      slippageBps: slippage,
-      payer,
-    });
-    quoteResponse = res.quoteResponse;
-    txid = await swapToken(res.swapTransaction, payer);
-  } catch (e) {
-    console.error(e);
-    bot.sendMessage(chatId, transactionBuildFailedMsg({ mode, isAuto }), {
-      parse_mode: 'HTML',
-      disable_web_page_preview: true,
-    });
-    return 'fail';
-  }
+  bot.sendMessage(chatId, await transactionInitiateMsg({ mode, isAuto }), {
+    parse_mode: 'HTML',
+  }).then(async ({ message_id }) => {
+    try {
+      const res = await initiateSwap({
+        inputMint,
+        outputMint,
+        amount: mode === 'buy' ? parseInt(amount * 0.99) : parseInt(amount),
+        slippageBps: slippage,
+        payer,
+      });
+      quoteResponse = res.quoteResponse;
+      txid = await swapToken(res.swapTransaction, payer);
 
-  try {
-    await confirmTransaction(txid);
+      await confirmTransaction(txid);
 
-    bot.editMessageText(transactionConfirmedMsg({ mode, isAuto, txid }), {
-      chat_id: chatId,
-      message_id,
-      parse_mode: 'HTML',
-      disable_web_page_preview: true,
-    });
+      bot.editMessageText(transactionConfirmedMsg({ mode, isAuto, txid }), {
+        chat_id: chatId,
+        message_id,
+        parse_mode: 'HTML',
+        disable_web_page_preview: true,
+      });
 
-    createTrade({
-      userId: chatId.toString(),
-      inputMint: quoteResponse.inputMint,
-      // inAmount: quoteResponse.inAmount,
-      inAmount: amount,
-      outputMint: quoteResponse.outputMint,
-      outAmount: parseInt(
-        quoteResponse.outAmount * (mode === 'buy' ? 1 : 0.99)
-      ),
-    });
+      createTrade({
+        userId: chatId.toString(),
+        inputMint: quoteResponse.inputMint,
+        inAmount: amount,
+        outputMint: quoteResponse.outputMint,
+        outAmount: parseInt(
+          quoteResponse.outAmount * (mode === 'buy' ? 1 : 0.99)
+        ),
+      });
 
-    if (
-      quoteResponse.inputMint ===
-      'So11111111111111111111111111111111111111112'
-    ) {
-      coverFee(chatId, amount / 100);
-    } else {
-      coverFee(chatId, quoteResponse.outAmount / 100);
+      if (quoteResponse.inputMint === 'So11111111111111111111111111111111111111112') {
+        await coverFee(chatId, amount / 100);
+      } else {
+        await coverFee(chatId, quoteResponse.outAmount / 100);
+      }
+      return 'success';
+    } catch (e) {
+      console.error(e);
+      bot.sendMessage(chatId, transactionFailedMsg({ mode, isAuto, txid }), {
+        parse_mode: 'HTML',
+        disable_web_page_preview: true,
+      });
+      return 'fail';
     }
-    return 'success';
-  } catch (e) {
-    console.error(e);
-    bot.sendMessage(chatId, transactionFailedMsg({ mode, isAuto, txid }), {
-      parse_mode: 'HTML',
-      disable_web_page_preview: true,
-    });
-    return 'fail';
-  }
-}
+  });
+};
 
 module.exports = {
   parseTransaction,
+  copyTrade,
 };
